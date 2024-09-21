5389511ccbf1f698c354f46c55f0eaf408408eb8
airotivmaria <mariavitoria6019@gmail.com>
2024-09-20 21:12:16 -0300

    Finalização da main, com adição das funções
    Signed-off-by: airotivmaria <mariavitoria6019@gmail.com>

--- DESAFIO1.1-master-master/.vscode/Main.java
+++ DESAFIO1.1-master-master/.vscode/Main.java
@@ -5,15 +5,17 @@ import java.util.Scanner;
 import infantil.escola.Alunos;
 import infantil.escola.Professor;
 import infantil.escola.Turma;
-import infantil.escola.escola;
+import infantil.escola.Professor.pagamento;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        Professor professor = new Professor();
         ArrayList<Alunos> alunos = new ArrayList<>();
         ArrayList<Professor> professores = new ArrayList<>();
         ArrayList<Turma> turmas = new ArrayList<>();
-        ArrayList<escola> TurmaAlunos = new ArrayList<>();
+        ArrayList<pagamento> pagamentos = professor.getPagamentos();
+        professor.pagamentoProfessor(professores);
 
         while (true) {
             mostrarMenu();
@@ -30,48 +32,65 @@ public class Main {
                 break;
             case "3":
                 cadastrarTurma(turmas);
+                System.out.println("Turma cadastrada com sucesso.");
                 break;
             case "4":
                 relatorios();
-                menuOpcao = scanner.nextLine();
-                if (menuOpcao == "1") {
+                System.out.print("Sua escolha: ");
+                String menuOpcao2 = scanner.nextLine();
+                switch (menuOpcao2) {
+                    case "1":
+                    System.out.print("Qual a turma: ");
+                    String opcaoTurma = scanner.nextLine().toUpperCase();
+                        switch (opcaoTurma){
+                            case "A":
+                                System.out.println("***Materiais Turma A***");
+                                materaisTurmaA();
+                                continue;
+                            case "B":
+                                System.out.println("***Materiais Turma B***");
+                                materiaisTurmaB();
+                                continue;
+                            case "C":
+                                System.out.println("***Materiais Turma C***");
+                                materiaisTurmaC();
+                                continue;
+                            default:
+                                System.out.println("Turma inválida");
+                                continue;}
+                    case "2":
                         System.out.print("Qual a turma: ");
-                        String opcaoTurma = scanner.nextLine().toUpperCase();
-                        if (opcaoTurma == "A"){
-                            System.out.println("***Materiais Turma A***");
-                            materaisTurmaA(scanner);
-                        } else if (opcaoTurma == "B") {
-                            System.out.println("***Materiais Turma B***");
-                            materiaisTurmaB();
-                        } else if (opcaoTurma == "C") {
-                            System.out.println("***Materiais Turma C***");
-                            materiaisTurmaC();
-                        } else {
-                            System.out.println("Turma inválida");
-                            break;
-                    }} else if (menuOpcao == "2"){
-                        System.out.println("Qual a turma: ");
                         String nomeTurma = scanner.nextLine();
-                        listarAlunos(TurmaAlunos, nomeTurma);
-                        break;
-                    } else if (menuOpcao == "3"){
+                        listarAlunos(alunos, nomeTurma);
+                        continue;
+                    case "3":
                         listarProfessor(professores);
-                    } else
-                break;
+                        continue;
+                    case "4":
+                        if(professores.size() != 0){
+                            professor.pagamentoProfessor(professores);
+                        } else {
+                            System.out.println("É preciso cadastrar um professor primeiro.");
+                        }
+                        continue;
+                    default:
+                        System.out.println("Opção inválida");
+                        continue;
+                }
             case "5":
+                System.out.println("Até a próxima!");
                 return;
-
             default:
             System.out.println("Opção inválida.");
                 break;
             }
         }
-
     }
 
+    //MENU INICIAL
     private static void mostrarMenu() {
         String menu = """
-                **********************
+                \n**********************
                 1 - Cadastrar aluno
                 2 - Cadastrar professor
                 3 - Cadastrar turma
@@ -82,6 +101,7 @@ public class Main {
         System.out.println(menu);
     }
 
+    // FUNÇÕES DE TODA PARTE DE CADASTRO, PUXA MÉTODOS DAS CLASSES ALUNOS, PROFESSORES E TURMAS
     private static void cadastrarAluno(ArrayList<Alunos> alunos) {
         Alunos aluno = new Alunos();
         aluno.cadastrar();
@@ -100,6 +120,7 @@ public class Main {
         turmas.add(turma);
     }
 
+    //MENU 2 DOS RELATÓRIOS DISPONÍVEIS
     private static void relatorios() {
         String menu2 = """
                 1 - Materiais que devem ser entreguem para os alunos
@@ -110,7 +131,8 @@ public class Main {
         System.out.println(menu2);
     }
 
-    private static void materaisTurmaA(Scanner scanner) {
+    //FUNÇÕES DOS MATERIAIS NECESSÁRIOS POR TURMA
+    private static void materaisTurmaA() {
         // TURMA A
         List<String> materialTurmaA = new ArrayList<>();
         List<String> materiaisA = List.of("Caderno", "Lapís", "Garrafa de água", "Agenda");
@@ -118,40 +140,55 @@ public class Main {
         for (String material : materialTurmaA) {
             System.out.println("-" + material);
         }
+        return;
     }
-
     private static void materiaisTurmaB() {
         // TURMA B
         List<String> materialTurmaB = new ArrayList<>();
         List<String> materiaisB = List.of("Fichário", "Caneta", "Mochila", "Estojo");
-        materiaisB.addAll(materiaisB);
+        materialTurmaB.addAll(materiaisB);
         for (String material : materialTurmaB) {
             System.out.println("-" + material);
         }
     }
-
     private static void materiaisTurmaC() {
         // TURMA C
-        List<String> materialTurmaC = new ArrayList<>();
+        List<String> materiaisTurmaC = new ArrayList<>();
         List<String> materiaisC = List.of("Livro", "Lapiseira", "Lancheira", "Avental de pintura");
-        materiaisC.addAll(materiaisC);
-        for (String material : materialTurmaC) {
+        materiaisTurmaC.addAll(materiaisC);
+        for (String material : materiaisC) {
             System.out.println("-" + material);
         }
     }
 
-    private static void listarAlunos(ArrayList<escola> TurmaAlunos, String nomeTurma) {
-        for (escola aluno : TurmaAlunos) {
-            if (escola.getNomeTurma().equals(nomeTurma.toUpperCase())){
-                System.out.println(escola.getNomeAluno());
+    //LISTA OS ALUNOS CADASTRADOS
+    private static void listarAlunos(ArrayList<Alunos> alunos, String nomeTurma) {
+        try {
+            if (alunos.size() != 0){
+                for (Alunos aluno : alunos) {
+                    if (Alunos.getNomeTurma().contains(nomeTurma)){
+                        System.out.println(aluno);
+                    }
+                }
+            }else{
+                System.out.println("A lista de Alunos, está vazia. Cadastre primeiro.");
             }
-            System.out.println(aluno);
+        } catch (Exception e) {
+            System.out.println("É necessário cadastrar a turma primeiro.");
         }
     }
 
+    //LISTA OS PROFESSORES CADASTRADOS
     private static void listarProfessor(ArrayList<Professor> professores){
-        for (Professor professor : professores){
-            System.out.println("-" + professor);
+        if (professores.size() != 0) {
+            for (Professor professor : professores) {
+                System.out.println("-" + professor);
+            }
+        } else {
+            System.out.println("A lista de professores, está vazia. Cadastre primeiro.");
         }
+        
     }
+
 }
+
